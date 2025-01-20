64be25e9c6037c72aa3049aa812f7df4413df593
Farasatullah <50820479+Farasatullah@users.noreply.github.com>
2025-01-20 11:24:32 +0500

    Initial Commit

---
+++ YouAreAwesome/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  YouAreAwesome
+//
+//  Created by Farasat's_MacBook_Pro on 20/01/2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ YouAreAwesome/YouAreAwesomeApp.swift
@@ -0,0 +1,17 @@
+//
+//  YouAreAwesomeApp.swift
+//  YouAreAwesome
+//
+//  Created by Farasat's_MacBook_Pro on 20/01/2025.
+//
+
+import SwiftUI
+
+@main
+struct YouAreAwesomeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
