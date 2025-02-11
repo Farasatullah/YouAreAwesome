eca12022b200f5b5bda1ef303d7d898a2eed0365
Farasatullah <50820479+Farasatullah@users.noreply.github.com>
2025-02-11 19:14:28 +0500

    Gone through Professors Lecture 1.5

--- YouAreAwesome/ContentView.swift
+++ YouAreAwesome/ContentView.swift
@@ -8,21 +8,56 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    //MARK: Created State varaibles
+    @State private var message = ""
+    @State private var imageName = ""
+
+    
     var body: some View {
-        
+
         VStack {
-            Image(systemName: "swift")
+            Spacer()
+
+            Image(systemName: imageName)
                 .resizable()
                 .scaledToFit()
                 .foregroundColor(.orange)
             
             
-            Text("I am an App Developer are Awesome!")
+            Text(message)
                 .font(.largeTitle)
                 .fontWeight(.heavy)
-                .foregroundStyle(.red)
-            
+                .foregroundStyle(.gray)
+            Spacer()
+            HStack{
+                Button("Press Me!") {
+                    print("button oressed")
+                    switch message {
+                    case "You are Awesome!":
+                        message = "You are Great!"
+                        imageName = "hand.thumbsup"
+                        break
+                    case "You are Great!":
+                        message = "You are Awesome!"
+                        imageName = "sun.max.fill"
+                        break
+                    default:
+                        print("empty so go with default values")
+                        message = "You are Awesome!"
+                        imageName = "sun.max.fill"
+
+                    }
+
+                }
+                .buttonStyle(.bordered)
+                .background(Color.orange)
+                .foregroundColor(.white)
+                .cornerRadius(10)
+                
+            }
         }
+
         .padding()
         
     }
